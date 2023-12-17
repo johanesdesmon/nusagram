0b22618f1ebc3703339c4acc4d55c4422e6acf3d
Desmon <jhnsdsmnkpys@gmail.com>
2023-12-17 22:33:57 +0700

    halaman about

--- app.py
+++ app.py
@@ -288,6 +288,9 @@ def secret():
 def inihome():
     return render_template('home.html')
 
+@app.route("/about", methods=['GET'])
+def iniabout():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
